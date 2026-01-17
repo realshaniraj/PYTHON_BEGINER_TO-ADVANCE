b5fb3f229d6ea33fe61990c43fbd5dcb033e13ee
shaniraj kmar <pandeys9032@gmail.com>
2026-01-17 19:50:21 +0530

    Simple Python program using if-else to simulate traffic light signals.

---
+++ traffic_light.py
@@ -0,0 +1,13 @@
+light=(input("enter coler :"))
+
+if(light == "red"):
+    print("stop")
+    
+elif(light == "yellow"):
+    print("redy")
+elif(light == "green"):
+    print("go")
+    
+    
+else:
+    print("light is broken")           
